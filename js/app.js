e1e8dc46dc1e77e8ab1f9fdfaea0600d42ad02d0
david silva <contacto@dvidsilva.com>
2016-08-04 16:37:19 -0700

    adds limit parameter to restrict the amount of pics displayed

--- js/app.js
+++ js/app.js
@@ -4,18 +4,23 @@
         Utils.request(CONFIG.IMGUR_API_ENDPOINT + gallery + '/viral/0.json', 
             {headers: [{name: 'Authorization', val: 'Client-ID 747f78a06dd424e'}]})
         .then(function (data) {
-              displayPictures(data.data);
+              displayPictures(data.data, 8);
         }, function (response){
             throw response.responseText;
         });
     }
 
-    function displayPictures(pictures) {
+    function displayPictures(pictures, limit) {
         var result = '';
+        limit = limit || 10;
+        var idx = 0;
         if (!Utils.isArray(pictures)) {
             throw 'tried to display pictures using invalid information';
         }
-        var result = pictures.reduce(function (pre, cur) {
+        result = pictures.reduce(function (pre, cur) {
+            if (idx >= limit) {
+                return pre;
+            }
             cur.img_url = cur.link;
             if (cur.nsfw === true ) {
                 return pre;
@@ -23,10 +28,11 @@
             if (cur.is_album === true) {
                 cur.img_url = 'http://imgur.com/' + cur.cover + '.jpg';
             }
+            idx++;
             return pre += Utils.parseTemplate('picture-frame', cur);
         }, '');
         document.getElementById('gallery-container').innerHTML = result;
     }
-
     getPictures();
 })();
+
